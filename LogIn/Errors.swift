55c5103f52fe270da913484dc5bd94c9804afd36
Pavel Mirkitanov <mirkitanov@mail.ru>
2021-07-01 00:29:40 +0300

    Добавлен функционал для создания нового пользователя и входа в систему

---
+++ LogIn/Errors.swift
@@ -0,0 +1,15 @@
+//
+//  Errors.swift
+//  Navigation
+//
+//  Created by Админ on 23.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+
+enum Errors: Error {
+    case incorrectData
+    case shortPassword
+    case incorrectEmail
+}
